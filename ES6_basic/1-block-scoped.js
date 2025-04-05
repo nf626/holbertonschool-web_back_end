76a8d093c9151ccfc32b5d082104e902c613acea
Nigel Feng <10083@holbertonstudents.com>
2025-04-05 12:23:17 +1100

    1. Block Scope

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,11 +1,12 @@
 export default function taskBlock(trueOrFalse) {
-  var task;
-  var task2;
 
   if (trueOrFalse) {
     var task = true;
     var task2 = false;
   }
 
+  var task = false;
+  var task2 = true;
+  
   return [task, task2];
 }
