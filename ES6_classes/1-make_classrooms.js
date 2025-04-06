ad331df36ad54273647c13e4c696dbba64105be4
Nigel Feng <10083@holbertonstudents.com>
2025-04-06 19:54:01 +1000

    1. Let's make some classrooms

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,8 +1,9 @@
 import ClassRoom from './0-classroom';
 
 function initializeRooms() {
-  const classArray = 19;
-  ClassRoom(classArray);
-}
+  const classroom1 = new ClassRoom(19);
+  const classroom2 = new ClassRoom(20);
+  const classroom3 = new ClassRoom(34);
 
-initializeRooms();
+  return [classroom1, classroom2, classroom3];
+}
