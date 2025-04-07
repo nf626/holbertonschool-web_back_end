624c3499a4aad8074f4738e6d187bee8270417a6
Nigel Feng <10083@holbertonstudents.com>
2025-04-07 14:58:42 +1000

    2. A Course, Getters, and Setters

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -17,11 +17,19 @@ class HolbertonCourse {
     this._students = students;
   }
 
+  get name() {
+    return this._name;
+  }
+
   set name(newName) {
     if (typeof newName !== 'string') {
       throw new Error('Name must be a string');
     }
     this._name = newName;
+  }
+
+  get length() {
+    return this._length
   }
 
   set length(newLength) {
@@ -31,23 +39,15 @@ class HolbertonCourse {
     this._length = newLength;
   }
 
+  get students() {
+    return this._students;
+  }
+
   set students(newStudents) {
     if (!Array.isArray(newStudents) || !newStudents.every((student) => typeof student === 'string')) {
       throw new Error('Students must be array of string');
     }
     this._students = newStudents;
-  }
-
-  get name() {
-    return this._name;
-  }
-
-  get length() {
-    return this._length
-  }
-
-  get students() {
-    return this._students;
   }
 }
 
