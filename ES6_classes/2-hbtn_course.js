4c4e4107590ef82e1aa946fb24ce9210334418a5
Nigel Feng <10083@holbertonstudents.com>
2025-04-07 14:55:12 +1000

    2. A Course, Getters, and Setters

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,52 +1,49 @@
 class HolbertonCourse {
   constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+
+    if (!Array.isArray(students) || !students.every((student) => typeof student === 'string')) {
+      throw new TypeError('Students must be array of string');
+    }
+
     this._name = name;
     this._length = length;
     this._students = students;
   }
 
-  set name(_newName) {
-    try {
-      if (typeof _newName === 'string') {
-        this._name = _newName;
-      } else {
-        throw new TypeError('Name must be a string');
-      }
-    } catch (error) {
-      console.log(error.message);
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new Error('Name must be a string');
     }
+    this._name = newName;
+  }
+
+  set length(newLength) {
+    if (typeof newLength !== 'number') {
+      throw new Error('Length must be a number');
+    }
+    this._length = newLength;
+  }
+
+  set students(newStudents) {
+    if (!Array.isArray(newStudents) || !newStudents.every((student) => typeof student === 'string')) {
+      throw new Error('Students must be array of string');
+    }
+    this._students = newStudents;
   }
 
   get name() {
     return this._name;
   }
 
-  set length(_newLength) {
-    try {
-      if (typeof _newLength === 'number') {
-        this._length = _newLength;
-      } else {
-        throw new TypeError('Length must be a number');
-      }
-    } catch (error) {
-      console.log(error.message);
-    }
-  }
-
   get length() {
-    return this._length;
-  }
-
-  set students(_newStudents) {
-    try {
-      if (Array.isArray(_newStudents)) {
-        this._students = _newStudents;
-      } else {
-        throw new TypeError('newStudents must be array');
-      }
-    } catch (error) {
-      console.log(error.message);
-    }
+    return this._length
   }
 
   get students() {
