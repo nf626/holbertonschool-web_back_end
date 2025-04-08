6a1e636154439ca83f38cc6ade6652b4db5e3c42
Nigel Feng <10083@holbertonstudents.com>
2025-04-08 17:55:31 +1000

    7. Airport

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,34 @@
+class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new Error('Name not string');
+    }
+    this._name = newName;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(newCode) {
+    if (typeof newCode !== 'string') {
+      throw new Error('Code not string');
+    }
+    this._code = newCode;
+  }
+
+  airportDescription() {
+    return `${this._code}`;
+  }
+}
+
+export default Airport;
