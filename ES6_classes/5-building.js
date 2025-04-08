487ad9899551280e96433f7c15a9fb6b8c7c6184
Nigel Feng <10083@holbertonstudents.com>
2025-04-08 14:00:37 +1000

    5. A Building

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,24 +1,19 @@
 class Building {
   constructor(sqft) {
     if (new.target === Building) {
-      throw new Error('Building cannot be instantiated directly');
+      throw new Error("Cannot instantiate an abstract class");
     }
-    this._sqft = sqft;
+    this._sqft = sqft; // underscore-prefixed variable
   }
 
+  // Getter for sqft
   get sqft() {
     return this._sqft;
   }
 
-  set sqft(newSqft) {
-    if (typeof newSqft !== 'number') {
-      throw new Error('newSqft is not number');
-    }
-    this._sqft = newSqft;
-  }
-
+  // Abstract method that must be implemented by subclasses
   evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
+    throw new Error("Class extending Building must override evacuationWarningMessage");
   }
 }
 
