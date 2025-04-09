9627c354036c95d46cab36ceb4ba17fcc1955b35
Nigel Feng <10083@holbertonstudents.com>
2025-04-10 00:51:32 +1000

    4. Combine

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 /* eslint-disable max-len */
 function updateStudentGradeByCity(getListStudents, city, newGrades) {
   const filterStudent = getListStudents.filter((student) => student.location === city);
