6f449bbd0c8643e9ac6b91f839f67904223a43be
Nigel Feng <10083@holbertonstudents.com>
2025-04-09 14:17:21 +1000

    1. More mapping

---
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,11 @@
+import getListStudents from "./0-get_list_students";
+
+function getListStudentIds(getListStudents) {
+  if (!Array.isArray(getListStudents)) {
+    return [];
+  }
+  return {
+  };
+}
+
+export default getListStudentIds;
