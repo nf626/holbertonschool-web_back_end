b5c8d34493e6e839e054b2b7e7d9a16567320f4c
Nigel Feng <10083@holbertonstudents.com>
2025-04-12 00:04:54 +1000

    10. More map data structure

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,4 +1,7 @@
 function updateUniqueItems(myMap) {
+  if (myMap instanceof Map) {
+    throw new Error('Cannot process');
+  }
   myMap.forEach((value, key) => {
     if (value === 1) {
       myMap.set(key, 100);
