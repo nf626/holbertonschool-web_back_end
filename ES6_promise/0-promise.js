d87a166a669bf095830b5a2746a493a68e0ff7e6
Nigel Feng <10083@holbertonstudents.com>
2025-04-12 00:44:43 +1000

    0. Keep every promise you make and only make promises you can keep

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,9 @@
+function getResponseFromAPI() {
+  const promise = new Promise((resolve, reject) => {
+    resolve();
+    reject();
+  });
+  return promise;
+}
+
+export default getResponseFromAPI;
