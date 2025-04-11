59efee839afb5fb2c38cb1c6c68eaf45b6f8af88
Nigel Feng <10083@holbertonstudents.com>
2025-04-11 13:22:51 +1000

    7. More set data structure

--- ES6_data_manipulation/7-has_array_values.js
+++ ES6_data_manipulation/7-has_array_values.js
@@ -1,7 +1,5 @@
 function hasValuesFromArray(set, array) {
   const arraySet = new Set(array);
-  console.log(arraySet);
-  console.log(arraySet.isSubsetOf(set));
   return arraySet.isSubsetOf(set);
 }
 
