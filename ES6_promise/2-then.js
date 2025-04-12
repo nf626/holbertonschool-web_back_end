21b6636a138ef40fde38f8b84655debb28400d73
Nigel Feng <10083@holbertonstudents.com>
2025-04-12 14:35:05 +1000

    2. Catch me if you can!

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,9 +1,7 @@
 function handleResponseFromAPI(promise) {
   const myPromise = promise
     .then(() => ({ status: 200, body: 'success' }))
-    .catch(() => {
-      throw new Error('');
-    })
+    .catch(() => new Error(''))
     .finally(() => {
       console.log('Got a response from the API');
     });
