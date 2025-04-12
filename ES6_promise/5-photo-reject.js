96645a34c582fa3f6fcc84c1a1e021bfeb672f29
Nigel Feng <10083@holbertonstudents.com>
2025-04-13 01:06:13 +1000

    5. Reject the promises

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -1,6 +1,8 @@
 export default function uploadPhoto(filename) {
   const myPromise = new Promise((reject) => {
-    reject(new Error(`${filename} cannot be processed`));
+    reject(
+      new Error(`${filename} cannot be processed`),
+    );
   });
   return myPromise;
 }
