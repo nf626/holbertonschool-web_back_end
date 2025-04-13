d323c662a27757891cc3c0ddd69dafa7b14fefba
Nigel Feng <10083@holbertonstudents.com>
2025-04-13 19:20:22 +1000

    8. Throw an error

--- ES6_promise/8-try.js
+++ ES6_promise/8-try.js
@@ -1,3 +1,3 @@
 export default function divideFunction(numerator, denominator) {
-  return denominator === 0 ? Error('cannot divide by 0') : numerator / denominator;
+  return denominator === 0 ? new Error('cannot divide by 0') : numerator / denominator;
 }
