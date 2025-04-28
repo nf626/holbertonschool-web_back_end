77c27e6052dd4ec2938e0c4e7013b28ed3c07881
Nigel Feng <10083@holbertonstudents.com>
2025-04-28 19:15:45 +1000

    0. Basic annotations - add

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
+''' print(__import__("0-add").my_function.__doc__)
+'''
 def add(a: float, b: float) -> float:
     return a + b
-
-'print(__import__("0-add").my_function.__doc__)'
