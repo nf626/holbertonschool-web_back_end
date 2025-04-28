3927a05e9dbbd028cd28833a0ca995e15fe16ef4
Nigel Feng <10083@holbertonstudents.com>
2025-04-28 22:04:22 +1000

    7. Complex types - string and int/float to tuple

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -8,6 +8,7 @@ the int/float v and should be annotated as a float.
 '''
 from typing import Tuple, Union
 
+
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     ''' return tuple '''
     return (k, v**2)
