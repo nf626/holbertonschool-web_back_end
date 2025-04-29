fa9b252a0d4501d6439f228ee0c02b2b3dc3dac0
Nigel Feng <10083@holbertonstudents.com>
2025-04-29 21:36:57 +1000

    4. Tasks

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -17,4 +17,4 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
 
     tasks = await asyncio.gather(*[wait_random(max_delay) for _ in range(n)])
 
-    return tasks
+    return sorted(tasks)
