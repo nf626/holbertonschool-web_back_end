4fa830f43c885a176bb5abd63647be133ae7c199
Nigel Feng <10083@holbertonstudents.com>
2025-05-01 18:23:21 +1000

    2. Hypermedia pagination

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -46,5 +46,17 @@ class Server:
         else:
             return data[page_start:page_end]
 
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[]:
-        pass
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> None:
+        ''' returns a dictionary containing the
+        following key-value pairs '''
+        data = self.dataset()
+        hyper_media = {
+            'page_size': page_size,
+            'page': page,
+            'data': self.get_page(page, page_size),
+            'next_page': page + 1,
+            'prev_page': page - 1,
+            'total_pages': len(data)
+        }
+
+        return hyper_media
