baf6d553eddbf097d2190f5313fd79542da96c7e
Nigel Feng <10083@holbertonstudents.com>
2025-05-04 00:35:42 +1000

    11. Where can I learn Python?

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''
+Write a Python function that returns the list of school having a specific topic:
+
+Prototype: def schools_by_topic(mongo_collection, topic):
+mongo_collection will be the pymongo collection object
+topic (string) will be topic searched
+'''
+
+
+def schools_by_topic(mongo_collection, topic):
+    ''' returns the list of school having a specific topic '''
+    if mongo_collection is not None:
+        find = mongo_collection.find_one( { "topic": topic } )
+        return find
