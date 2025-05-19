a58fcc2ba951accfc7382955376164eff5a3c341
Nigel Feng <10083@holbertonstudents.com>
2025-05-19 14:07:36 +1000

    1. Using Process stdin

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,14 +1,14 @@
-process.stdout.write('Welcome to Holberton School, what is your name? ' + '\n');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.setEncoding('utf8');
 
 process.stdin.on('readable', function() {
   var chunk = process.stdin.read();
   if (chunk !== null) {
-    process.stdout.write('Your name is: ' + chunk);
+    process.stdout.write(`Your name is: ${chunk}`);
   }
 });
 
 process.stdin.on('end', function() {
-  process.stdout.write('This important software is now closing ' + '\n');
+  process.stdout.write('This important software is now closing\n');
 });
