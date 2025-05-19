14b2f73f5ef7b111c727f6449ace59078cee751b
Nigel Feng <10083@holbertonstudents.com>
2025-05-19 13:06:42 +1000

    0. Executing basic javascript with Node JS

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,8 +1,3 @@
-/*
-In the file 0-console.js, create a function named displayMessage
-that prints in STDOUT the string argument.
- */
-
 function displayMessage(message) {
   console.log(message);
 }
