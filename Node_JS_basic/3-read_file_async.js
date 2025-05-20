7e7fc556122126a73c5bffc758ef3c57b8b5a0ae
Nigel Feng <10083@holbertonstudents.com>
2025-05-20 17:32:10 +1000

    3. Reading a file asynchronously with Node JS

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -1,45 +1,46 @@
-// Async (Non-blocking) - use readFile
 const fs = require('fs');
 
 function countStudents(path) {
   return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf-8', (err, data) => {
+    fs.readFile(path, 'utf8', (err, data) => {
       if (err) {
         reject(Error('Cannot load the database'));
+        return;
       }
-
       const response = [];
-      let message;
+      let msg;
 
       const content = data.toString().split('\n');
 
       let students = content.filter((item) => item);
 
-      students = students.map((item) => item.split(', '));
+      students = students.map((item) => item.split(','));
 
       const NUMBER_OF_STUDENTS = students.length ? students.length - 1 : 0;
-      message = `Number of students: ${NUMBER_OF_STUDENTS}`;
-      console.log(message);
+      msg = `Number of students: ${NUMBER_OF_STUDENTS}`;
+      console.log(msg);
 
-      response.push(message);
+      response.push(msg);
 
       const fields = {};
-      for (const student in students) {
-        if (student !== 0) {
-          if (!fields[students[student][3]]) {
-            fields[students[student][3]] = [];
-          }
-          fields[students[student][3]].push(students[student][0]);
+      for (const i in students) {
+        if (i !== 0) {
+          if (!fields[students[i][3]]) fields[students[i][3]] = [];
+
+          fields[students[i][3]].push(students[i][0]);
         }
       }
 
       delete fields.field;
 
       for (const key of Object.keys(fields)) {
-        message = `Number of students in ${key}:
-        ${fields[key].length}. List: ${fields[key].join(', ')}`;
-        console.log(message);
-        response.push(message);
+        msg = `Number of students in ${key}: ${
+          fields[key].length
+        }. List: ${fields[key].join(', ')}`;
+
+        console.log(msg);
+
+        response.push(msg);
       }
       resolve(response);
     });
