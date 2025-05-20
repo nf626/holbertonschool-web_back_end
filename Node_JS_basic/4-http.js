1f1585aa6e17b0a34993a146f506aa563171cac6
Nigel Feng <10083@holbertonstudents.com>
2025-05-20 19:15:16 +1000

    4. Create a small HTTP server using Node's HTTP module

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -2,7 +2,8 @@ const http = require('http');
 
 const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello Holberton School!');
+  res.write('Hello Holberton School!');
+  res.end();
 }).listen(1245);
 
 module.exports = app;
