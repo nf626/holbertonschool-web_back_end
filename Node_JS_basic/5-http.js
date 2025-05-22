6b4d22ffbd5fd9c8afe8f0864484d6ca77312762
Nigel Feng <10083@holbertonstudents.com>
2025-05-22 18:54:23 +1000

    5. Create a more complex HTTP server using Node's HTTP module

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const countStudents = require('./3-read_file_async');
+
+const Port = 1245;
+
+const consoleArg = process.argv.slice(2);
+const db = consoleArg[0];
+
+const app = http.createServer(async (req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-type': 'text/plain' });
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    res.writeHead(200, { 'Content-type': 'text/plain' });
+    res.write('This is the list of our students');
+    try {
+      const data = await countStudents(db);
+      res.end(`${data.join('\n')}`);
+    } catch (err) {
+      res.end(err.message);
+    }
+  }
+}).listen(Port);
+
+module.exports = app;
