d1d7b8db7fcc730abb92f977b2f78fc7cbdc3e04
Nigel Feng <10083@holbertonstudents.com>
2025-05-25 12:38:52 +1000

    7. Create a more complex HTTP server using Express

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -12,9 +12,18 @@ app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.get('/students', (req, res) => {
-  res.send('This is the list of our students');
-  res.send();
+app.get('/students', async (req, res) => {
+  try {
+    const data = await countStudents(db);
+    const msg = {
+      message1: 'This is the list of our students',
+      message2: `${data.join('\n')}`,
+    };
+
+    res.send(JSON.stringify(msg));
+  } catch (err) {
+    res.send(err.message);
+  }
 });
 
 app.listen(port, () => {
